a4f1e74f4a3241306b1289656d8bf3109343e939
Jim Jeon <jimjeon.dev@gmail.com>
2024-03-05 18:51:47 +0900

    Add generate token and send login email

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI, Depends, HTTPException
+from datetime import datetime, timedelta
+import jwt
+import smtplib
+from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from typing import List
+from fastapi import FastAPI, Depends, HTTPException, Request
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from random import choice
 from string import ascii_letters
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 from redis.exceptions import WatchError
+from pydantic_settings import BaseSettings
 
 from db import SessionLocal, PetitLink, redis_client
 
 
+class AuthSettings(BaseSettings):
+    auth_email: str
+    auth_email_password: str
+    auth_secret_key: str
+    auth_salt: str
+    auth_access_token_secret_key: str
+
+
+settings = AuthSettings()
+templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 
 
@@ -32,7 +51,7 @@ class UpdateLinkDto(BaseModel):
 
 
 @app.get('/index')
-async def index():
+async def index(request: Request):
     return 'hello world'
 
 
@@ -109,3 +128,92 @@ async def redirect(path: str):
     else:
         return RedirectResponse(link)
 
+
+class LoginForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.errors: List = []
+        self.username: str | None = None
+
+    async def load_data(self):
+        form = await self.request.form()
+        self.username = form.get('email')
+
+    async def is_valid(self):
+        if not self.username or not (self.username.__contains__('@')):
+            self.errors.append('Email is required')
+        if not self.errors:
+            return True
+        return False
+
+
+@app.get('/login')
+def login(request: Request):
+    return templates.TemplateResponse("auth/login.html", {"request": request})
+
+
+@app.post('/login')
+async def login(request: Request, db: Session = Depends(get_db)):
+    form = LoginForm(request)
+    await form.load_data()
+    if await form.is_valid():
+        try:
+            # TODO: Send login email
+            await send_login_email(form.username)
+            form.__dict__.update(msg='Login Email Sent')
+            response = templates.TemplateResponse('auth/login.html', form.__dict__)
+            return response
+        except HTTPException:
+            form.__dict__.update(msg='')
+            return templates.TemplateResponse('auth/login.html', form.__dict__)
+    return templates.TemplateResponse('auth/login.html', form.__dict__)
+
+
+async def send_login_email(to: str):
+    msg = MIMEMultipart()
+    msg['Subject'] = 'PetitLink Login'
+    msg['From'] = settings.auth_email
+    msg['To'] = to
+
+    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
+    link = serializer.dumps(to, salt=settings.auth_salt)
+    link = 'http://local.petitlink.com:8000/verify/' + link
+
+    msg.attach(MIMEText(f'''
+    <a href="{link}">Login</a>
+    ''', 'html'))
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()  # TLS support for security
+        server.login(settings.auth_email, settings.auth_email_password)
+        server.sendmail(settings.auth_email, to, msg.as_string())
+        print('email sent successfully')
+
+
+@app.get('/verify/{token}')
+async def verify(token: str, expiration: int = 600):
+    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
+    try:
+        email = serializer.loads(token, salt=settings.auth_salt, max_age=expiration)
+        token = generate_access_token(email)
+        response = RedirectResponse('/index')
+        response.set_cookie(
+            key='token', value=token, httponly=True, samesite='strict', domain='petitlink.com')
+        return response
+    except BadSignature or SignatureExpired:
+        return False
+
+
+def generate_access_token(email: str):
+    payload = {
+        'email': email,
+        'exp': datetime.now() + timedelta(hours=24)
+    }
+
+    token = jwt.encode(
+        payload,
+        settings.auth_access_token_secret_key,
+        algorithm='HS256'
+    ).encode('utf-8')
+
+    return token
