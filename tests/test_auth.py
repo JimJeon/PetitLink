4943ae392f299c0c035ff0b1e3269110ed10c0f0
Jim Jeon <jimjeon.dev@gmail.com>
2024-03-06 03:16:57 +0900

    Add test for send_login_email

---
+++ tests/test_auth.py
@@ -0,0 +1,32 @@
+import pytest
+
+from main import send_login_email, AuthSettings
+
+
+class TestSendLoginEmail:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        'patch_settings',
+        [
+            {'auth_email': 'from@test.com', 'auth_email_password': 'auth-email-password'},
+        ],
+        indirect=True
+    )
+    async def test_send_login_email(self, mocker, patch_settings: AuthSettings):
+        # Arrange
+        mock_smtp = mocker.MagicMock(name='main.smtplib.SMTP')
+        mocker.patch('main.smtplib.SMTP', new=mock_smtp)
+
+        # Act
+        await send_login_email('to@test.com')
+
+        # Assert
+        mock_smtp.return_value.__enter__.return_value.starttls.assert_called_once()
+        mock_smtp.return_value.__enter__.return_value.login.assert_called_once()
+        mock_smtp.return_value.__enter__.return_value.sendmail.assert_called_once()
+
+        mock_smtp.assert_called_with('smtp.gmail.com', 587)
+        mock_smtp.return_value.__enter__.return_value.login.assert_called_with('from@test.com', 'auth-email-password')
+        # mock_smtp.return_value.__enter__.return_value.sendmail.assert_called_with('from@test.com', 'to@test.com')
+
+
