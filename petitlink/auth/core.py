6703beff104015f55f67a83160c46d617a4223db
Jim Jeon <jimjeon.dev@gmail.com>
2024-03-13 20:20:45 +0900

    Fix auth api

--- petitlink/auth/core.py
+++ petitlink/auth/core.py
@@ -6,42 +6,12 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from argon2 import PasswordHasher
-from argon2.exceptions import VerificationError, HashingError
-from fastapi import HTTPException, Depends
-from itsdangerous import URLSafeTimedSerializer
+from argon2.exceptions import HashingError
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from app import settings
 from models import UserTable
-
-
-def build_email_message(to: str) -> MIMEMultipart:
-    msg = MIMEMultipart()
-
-    # Add a message header
-    msg['Subject'] = 'PetitLink Login'
-    msg['From'] = settings.auth_email
-    msg['To'] = to
-
-    # Create a link with serialized email
-    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
-    link = serializer.dumps(to, salt=settings.auth_salt)
-    link = 'http://local.petitlink.com:8000/verify/' + link
-
-    # Add a message body
-    msg.attach(MIMEText(f'''
-        <a href="{link}">Login</a>
-        ''', 'html'))
-
-    return msg
-
-
-async def send_login_email(to: str, msg: MIMEMultipart) -> None:
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()  # TLS support for security
-        server.login(settings.auth_email, settings.auth_email_password)
-        server.sendmail(settings.auth_email, to, msg.as_string())
-        print('email sent successfully')
 
 
 def generate_access_token(email: str):
@@ -73,9 +43,7 @@ def decode_access_token(token: str) -> str:
 
 
 def register(db: Session, email: str, password: str):
-    hashed = hash_password(password)
-    new_user = UserTable(email=email, password_hash=hashed)
-
+    new_user = UserTable(email=email, password_hash=hash_password(password))
     db.add(new_user)
     db.commit()
 
@@ -90,15 +58,3 @@ def hash_password(password: str):
         print(e)
         return ''
     return _hash
-
-
-def verify_password(_hash: str, password: str):
-    ph = PasswordHasher()
-    try:
-        ph.verify(_hash, password)
-    except VerificationError:
-        return False
-    except Exception as e:
-        print(e)
-        return False
-    return True
--- petitlink/auth/routes.py
+++ petitlink/auth/routes.py
@@ -1,76 +1,37 @@
 from typing import List
 
-from fastapi import Request, HTTPException, Depends
-from fastapi.responses import RedirectResponse
-from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
+from fastapi import Request, Depends, status
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
-from app import app, templates, settings
+from app import app, templates
 from models import UserTable, get_db
 from core import (
-    build_email_message, send_login_email, generate_access_token, register
+    generate_access_token, register
 )
 
 
-class LoginForm:
-    def __init__(self, request: Request):
-        self.request: Request = request
-        self.errors: List = []
-        self.username: str | None = None
-
-    async def load_data(self):
-        form = await self.request.form()
-        self.username = form.get('email')
-
-    async def is_valid(self):
-        if not self.username or not (self.username.__contains__('@')):
-            self.errors.append('Email is required')
-        if not self.errors:
-            return True
-        return False
-
-
-@app.get('/login')
-async def login_get_view(request: Request):
-    return templates.TemplateResponse("auth/login.html", {"request": request})
-
-
 @app.post('/login')
-async def login_post_view(request: Request):
-    form = LoginForm(request)
-    await form.load_data()
-    if await form.is_valid():
-        try:
-            msg = build_email_message(form.username)
-            await send_login_email(form.username, msg)
-            form.__dict__.update(msg='Login Email Sent')
-            response = templates.TemplateResponse('auth/login.html', form.__dict__)
-            return response
-        except HTTPException:
-            form.__dict__.update(msg='')
-            return templates.TemplateResponse('auth/login.html', form.__dict__)
-    return templates.TemplateResponse('auth/login.html', form.__dict__)
-
-
-@app.get('/verify/{token}')
-async def verify(token: str, expiration: int = 600, db: Session = Depends(get_db)):
-    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
-
-    try:
-        email = serializer.loads(token, salt=settings.auth_salt, max_age=expiration)
-    except BadSignature or SignatureExpired:
-        return False
+async def login_post_handler(request: Request, db: Session = Depends(get_db)):
+    user_data = await request.json()
+    email = user_data['email']
+    password = user_data['password']
 
     user = UserTable.find_user_by_email(db, email)
 
-    if not user:
-        return RedirectResponse(f'/register/{token}')
+    if user.verify_password(password):
+        token = generate_access_token(email)
+        return JSONResponse(status_code=status.HTTP_200_OK, content={'token': token})  # Access Granted
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED, content={'message': 'Wrong password'})  # Access Denied
 
-    token = generate_access_token(email)
-    response = RedirectResponse('/index')
-    response.set_cookie(
-        key='token', value=token, httponly=True, samesite='strict', domain='petitlink.com')
-    return response
+
+@app.post('/register')
+async def register_post_handler(request: Request, db: Session = Depends(get_db)):
+    user_data = await request.json()
+    email = user_data['email']
+    password = user_data['password']
+    register(db, email, password)
 
 
 @app.get('/logout')
@@ -79,48 +40,3 @@ def logout(request: Request) -> None:
     response = templates.TemplateResponse('auth/login.html', {'request': request, 'msg': msg})
     response.delete_cookie(key='token', domain='petitlink.com')
     return response
-
-
-class RegisterForm:
-    def __init__(self, request: Request):
-        self.request: Request = request
-        self.errors: List = []
-        self.password: str | None = None
-
-    async def load_data(self):
-        form = await self.request.form()
-        self.password = form.get('password')
-
-    async def is_valid(self):
-        if len(self.password) <= 8:
-            self.errors.append('Password must be longer than 8 characters')
-        if not self.errors:
-            return True
-        return False
-
-
-@app.get('/register/{token}')
-async def register_get_view(request: Request, token: str, expiration: int = 1200):
-    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
-    try:
-        email = serializer.loads(token, salt=settings.auth_salt, max_age=expiration)
-    except BadSignature or SignatureExpired:
-        return False
-
-    return templates.TemplateResponse('auth/register.html', {'request': request})
-
-
-@app.post('/register/{token}')
-async def register_post_view(request: Request, token: str, expiration: int = 1200, db: Session = Depends(get_db)):
-    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
-    try:
-        email = serializer.loads(token, salt=settings.auth_salt, max_age=expiration)
-    except BadSignature or SignatureExpired:
-        return False
-
-    form = RegisterForm(request)
-    await form.load_data()
-    if await form.is_valid():
-        register(db, email, form.password)
-        return True
-    return templates.TemplateResponse('auth/register.html', form.__dict__)
