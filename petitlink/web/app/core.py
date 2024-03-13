af583e3f15d790ab819f0d5799a5ac4178898a28
Jim Jeon <jimjeon.dev@gmail.com>
2024-03-14 05:20:44 +0900

    Fix tests

--- petitlink/web/app/core.py
+++ petitlink/web/app/core.py
@@ -13,12 +13,12 @@ def build_email_message(to: str) -> MIMEMultipart:
 
     # Add a message header
     msg['Subject'] = 'PetitLink Login'
-    msg['From'] = settings.auth_email
+    msg['From'] = settings.petitlink_email
     msg['To'] = to
 
     # Create a link with serialized email
-    serializer = URLSafeTimedSerializer(settings.auth_secret_key)
-    link = serializer.dumps(to, salt=settings.auth_salt)
+    serializer = URLSafeTimedSerializer(settings.petitlink_email_secret_key)
+    link = serializer.dumps(to, salt=settings.petitlink_email_salt)
     link = 'http://petitlink.com:8000/login/' + link
 
     # Add a message body
@@ -32,6 +32,6 @@ def build_email_message(to: str) -> MIMEMultipart:
 async def send_login_email(to: str, msg: MIMEMultipart) -> None:
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()  # TLS support for security
-        server.login(settings.auth_email, settings.auth_email_password)
-        server.sendmail(settings.auth_email, to, msg.as_string())
+        server.login(settings.petitlink_email, settings.petitlink_email_pass)
+        server.sendmail(settings.petitlink_email, to, msg.as_string())
         print('email sent successfully')
